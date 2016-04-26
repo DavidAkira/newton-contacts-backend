bc4e82bc9aa011bbbda186cd5757aeb3290f328e
David Kihlstrand <david.kihlstrand@outlook.com>
2016-04-26 11:41:49 +0200

    Cleanup

--- NewtonContactsApp/AddNewContactPage.xaml.cs
+++ NewtonContactsApp/AddNewContactPage.xaml.cs
@@ -1,24 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Graphics.Imaging;
-using Windows.Networking.Sockets;
 using Windows.Storage;
 using Windows.Storage.Pickers;
-using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 using NewtonContactsApp.Model;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
--- NewtonContactsApp/GridViewPage.xaml.cs
+++ NewtonContactsApp/GridViewPage.xaml.cs
@@ -1,28 +1,13 @@
 ﻿using NewtonContactsApp.Model;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace NewtonContactsApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class GridViewPage : Page
     {
         private ObservableCollection<Contact> Contacts { get; }
@@ -104,7 +89,6 @@ namespace NewtonContactsApp
             CloseEdit();
             
         }
-
         private void UpdateUserTextboxes()
         {
             Contact updatedUser = MockContactsRepo.DbInstance.Get(CurrentContactIndex);
